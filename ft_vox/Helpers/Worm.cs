3a3b0ffb88163335f76bc99b366c1d3305f98891
Entrivax <entrivax@gmail.com>
2018-02-08 23:29:14 +0100

    Add Worm class

---
+++ ft_vox/Helpers/Worm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ft_vox.Helpers
+{
+    // https://github.com/Maxopoly/Caveworm/blob/master/src/main/java/com/github/maxopoly/caveworm/worms/SimplexNoiseWorm.java
+    public class Worm
+    {
+        
+        public Worm(int x, int y, int z,
+                    int xOctaves, int yOctaves, int zOctaves,
+                    float xFrequency, float yFrequency, float zFrequency,
+                    float xThreshold, float yThreshold, float zThreshold,
+                    float xAmplitude, float yAmplitude, float zAmplitude,
+                    int xSeed, int ySeed, int zSeed,
+                    int maxLength)
+        {
+
+        }
+    }
+}
